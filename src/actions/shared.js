506ba95f8961a6a1d1a43ec3eb3313315e782928
adengz <adengz@gmail.com>
2020-08-25 14:43:06 +0800

    feat: Add actions receiving initial data

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import * as API from '../utils/_DATA';
+import { receiveUsers } from './users';
+import { receiveQuestions } from './questions';
+
+export const handleInitialData = () => {
+  return async (dispatch) => {
+    dispatch(showLoading());
+
+    const users = await API._getUsers();
+    const questions = await API._getQuestions();
+    dispatch(receiveUsers(users));
+    dispatch(receiveQuestions(questions));
+    dispatch(hideLoading);
+  };
+};
