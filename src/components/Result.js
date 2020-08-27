ce2725a8faa7a5428addf150c0a011fb75a41fa2
adengz <adengz@gmail.com>
2020-08-27 11:34:12 +0800

    feat: Add result ui

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import '../styles/card.scss';
 
 export default function Result({ name, avatarURL, options, answer }) {
-  return <div>Result</div>;
+  const totalVotes = Object.values(options)
+    .map((o) => o.votes.length)
+    .reduce((a, b) => a + b);
+  const data = {};
+  Object.entries(options).forEach(([k, v]) => {
+    const { votes, text } = v;
+    const { length } = votes;
+    data[k] = {
+      text,
+      votes: length,
+      percent: (length / totalVotes) * 100,
+    };
+  });
+
+  return (
+    <div className="card">
+      <h3 className="title">{`Asked by ${name}`}</h3>
+      <div className="content">
+        <img className="avatar" src={avatarURL} alt={`${name}'s avatar`} />
+        <div className="detail">
+          <p className="bold">Result</p>
+          <p>Vote Bar</p>
+          {Object.entries(data).map(([k, v]) => (
+            <p key={k} className={k === answer ? 'mine' : null}>
+              {v.votes} / {totalVotes} ({v.percent}%) would rather {v.text}
+              {k === answer && (
+                <div className="badge">
+                  Your
+                  <br />
+                  Choice
+                </div>
+              )}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
